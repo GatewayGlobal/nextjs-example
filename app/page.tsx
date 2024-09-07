f66b5a100d7e2ab1c1397bf44bd6ac1d2118e1e3
GatewayGlobal <leonakal8614@gmail.com>
2024-09-07 21:17:09 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [createAccountUrl, setCreateAccountUrl] = useState<string>(null);
+  const [createAccountUrl, setCreateAccountUrl] = useState<string>('');
 
   useEffect(() => {
     setCreateAccountUrl(
